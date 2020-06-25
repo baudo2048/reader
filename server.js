3e440273555e9fcb251ea5ac68a324d994764055
BAUDO GIUSEPPE <U416183@sede.corp.sanpaoloimi.com>
2020-06-25 10:51:26 +0200

    mobile header

--- server.js
+++ server.js
@@ -3,71 +3,48 @@ const path = require('path');
 
 const http = require('http')
 const app = express();
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 const port = process.env.PORT || 3000;
 
 const WebSocket = require('ws')
 
 const server = http.createServer(app)
 
-const home = 'C:/Users/u416183/lab/work/py/htmlLang/htmlLang'
-const python = 'C:/Users/u416183/lab/work/py/htmlLang/htmlLang/infrastracture/python/python37emb/python.exe'
+const home = __dirname; //'C:/Users/u416183/lab/work/py/htmlLang/htmlLang'
+const python = path.join(__dirname, 'infrastracture') //'C:/Users/u416183/lab/work/py/htmlLang/htmlLang/infrastracture/python/python37emb/python.exe'
 
 const wss = new WebSocket.Server({ server: server })
 
-wss.on('connection', socket => {
-    console.log('connection...')
-    socket.on('message', msg => {
-        const req = JSON.parse(msg)
-        console.log('Message is ' + JSON.stringify(req))
-        if(req.eventName == 'socket.test.request'){
-            console.log('test service + ' + JSON.stringify(req.data))
-            socket.send(JSON.stringify({eventName: 'socket.test.response', data: 'hello'}))
-            return
-        }
-
-        if(req.eventName == 'requestCompile'){
-            const fileName = req.data.fileName
-            const code = req.data.code;
-
-            (async () => {
-                await fs.writeFile(`${home}/app/ux/${fileName}`, code, (err)=>{
-                    if(err) {
-                        console.log('writing error ' + err);
-                    } else {
-                        console.log('writing success');
-                    }    
-                });
-        
-                await exec(`${python} ${home}/lang/parser.py ${fileName.substr(0,fileName.length-3)}`, (error, stdout, stderr) => {
-                    if (error) {
-                        console.log(`error: ${error.message}`);
-                        return;
-                    }
-                    if (stderr) {
-                        console.log(`stderr: ${stderr}`);
-                        return;
-                    }
-                    console.log(`stdout: ${stdout}`);
-                    socket.send(JSON.stringify({eventName: "socket.compiler.done"}))
-                });
-        
-            })();
-        }
-    })
-})
 
 
 const fs = require('fs');
 const { exec } = require("child_process");
 
 
-app.get('/', (req, res) => res.send('Hello World! BiP'));
+app.get('/', (req, res) => {
 
-app.get('/compile', (req, res) => {
-    console.log('saving... ' + req.query.code);
+    console.log(JSON.stringify(req.headers))
+
+    res.send(JSON.stringify(req.headers))
+
+    req.header('User-Agent')
+
+
+    //res.set('Content-Type', 'text/html')
+
+
+    //res.sendFile(path.join(`${__dirname}/index.html`));
+});
+
+app.post('/requestCompile', (req, res, next) => {
+    
+    console.log('saving... ' + req.body.eventName +'  -  ' + JSON.stringify(req.body.data));
 
     (async () => {
-        await fs.writeFile('test.ux', req.query.code, (err)=>{
+        console.log('msg.fileName: ' + req.body.fileName)
+        await fs.writeFile('test.ux', req.body.data.code, (err)=>{
             if(err) {
                 console.log('writing error ' + err);
             } else {
@@ -90,37 +67,8 @@ app.get('/compile', (req, res) => {
         });
 
     })();
-});
+})
 
-app.post('/compile', (req, res) => {
-    console.log('saving... ' + req.query.body);
-
-    (async () => {
-        console.log('body: ' + req.query.code)
-        await fs.writeFile('test.ux', req.body, (err)=>{
-            if(err) {
-                console.log('writing error ' + err);
-            } else {
-                console.log('writing success');
-            }    
-        });
-
-        await exec(`python parser.py test`, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            console.log('ok')
-            res.send('ok')
-        });
-
-    })();
-});
 
 //app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
@@ -128,5 +76,6 @@ server.listen(process.env.PORT || 3000, () => {
     console.log(`Server started on port ${server.address().port} :)`);
 });
 
+console.log("__dirname is + " + __dirname)
 app.use(express.static(__dirname));
 
